ebb991a5845bd9c855019376f2d7386c9fffbe8a
cntchen <cntchen@gmail.com>
2020-05-03 14:11:33 +0800

    feat: add mobile react

---
+++ src/mobile/container/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function App() {
+    return <div>Demo Text</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
