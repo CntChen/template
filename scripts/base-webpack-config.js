69485dda1948997596467562ee18b96acad7afb2
cntchen <cntchen@tencent.com>
2020-05-12 00:18:20 +0800

    build: add image loader

--- scripts/base-webpack-config.js
+++ scripts/base-webpack-config.js
@@ -78,6 +78,28 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            // 图片资源抽离为独立文件的阈值
+                            limit: 4096,
+                            fallback: 'file-loader',
+                            // 用于 file-loader 的配置
+                            name: isProduction ? `[name]${HASH}.[ext]` : '[name].[ext]',
+                            outputPath: 'image',
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                          disable: !isProduction,
+                        },
+                    },
+                ],
+            },
         ],
     },
     plugins: [
